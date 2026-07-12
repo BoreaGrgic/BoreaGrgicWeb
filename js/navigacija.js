26c87afc5d4e07c0f74783f98693e1107a6bec88
BoreaGrgic <drago.grgic2@st.t-com.hr>
2026-07-12 14:20:15 +0200

    slike6

--- js/navigacija.js
+++ js/navigacija.js
@@ -1,45 +1,44 @@
-var navHTML = '<header>' +
-    '<nav>' +
-        '<div class="logo">' +
-            '<a href="index.html"><img src="assets/logo.png" alt="Logo" class="nav-logo-img"></a>' +
-        '</div>' +
-        '<div class="menu-toggle" id="mobile-menu">' +
-            '<span></span>' +
-            '<span></span>' +
-            '<span></span>' +
-        '</div>' +
-        '<ul class="nav-links" id="nav-list">' +
-            '<li><a href="index.html">Home</a></li>' +
-            '<li>' +
-                '<a href="wood-epoxy.html">Wood & Epoxy</a>' +
-                '<ul class="dropdown-menu">' +
-                    '<li><a href="wood-epoxy.html#products">Products</a></li>' +
-                    '<li><a href="wood-epoxy.html#hungry-hearts">Hungry Hearts</a></li>' +
-                    '<li><a href="wood-epoxy.html#gin-epoxy">Gin & Epoxy</a></li>' +
-                    '<li><a href="wood-epoxy.html#custom">Custom</a></li>' +
-                '</ul>' +
-            '</li>' +
-            '<li>' +
-                '<a href="software.html">Software</a>' +
-                '<ul class="dropdown-menu">' +
-                    '<li><a href="probexyz.html">ProbeXYZ - UCCNC plugin</a></li>' +
-                '</ul>' +
-            '</li>' +
-            '<li><a href="about.html">About</a></li>' +
-            '<li><a href="contact.html">Contact</a></li>' +
-        '</ul>' +
-    '</nav>' +
-'</header>';
+(function() {
+    var navHTML = `
+    <header>
+        <nav>
+            <div class="logo">
+                <a href="index.html"><img src="assets/logo.png" alt="Logo"></a>
+            </div>
+            <div class="menu-toggle" id="mobile-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul class="nav-links" id="nav-list">
+                <li><a href="index.html">Home</a></li>
+                <li>
+                    <a href="wood-epoxy.html">Wood & Epoxy</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="wood-epoxy.html#products">Products</a></li>
+                        <li><a href="wood-epoxy.html#gallery">Gallery</a></li>
+                        <li><a href="wood-epoxy.html#custom">Custom</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="software.html">Software</a>
+                    <ul class="dropdown-menu">
+                        <li><a href="probexyz.html">ProbeXYZ - UCCNC plugin</a></li>
+                    </ul>
+                </li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>`;
 
-// Ubacivanje bez čekanja DOM-a da stilovi odmah sjednu na mjesto
-document.body.insertAdjacentHTML('afterbegin', navHTML);
+    document.body.insertAdjacentHTML('afterbegin', navHTML);
 
-// Hamburger izbornik se veže odmah
-var menuToggle = document.getElementById('mobile-menu');
-var navLinks = document.getElementById('nav-list');
-
-if (menuToggle && navLinks) {
-    menuToggle.addEventListener('click', function() {
-        navLinks.classList.toggle('active');
-    });
-}
+    var menuToggle = document.getElementById('mobile-menu');
+    var navLinks = document.getElementById('nav-list');
+    if (menuToggle && navLinks) {
+        menuToggle.addEventListener('click', function() {
+            navLinks.classList.toggle('active');
+        });
+    }
+})();
